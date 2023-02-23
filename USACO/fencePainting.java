80a6eb0e3619c8a0e055d6f87dcc1a998bccd03c
luizademelo <ludemelo5452@gmail.com>
2023-02-23 15:04:05 -0300

    creating usaco folder

---
+++ USACO/fencePainting.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException; 
+
+public class fencePainting {
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+
+        int a,b,c,d; 
+        File file = new File("paint.in"); 
+        Scanner scan = new Scanner(file); 
+        a = scan.nextInt(); 
+        b = scan.nextInt(); 
+        scan.nextLine(); 
+        c = scan.nextInt(); 
+        d = scan.nextInt(); 
+        scan.close(); 
+        File out = new File("paint.out"); 
+        FileWriter writer = new FileWriter(out); 
+        int painted = (b-a) + (d-c) - Math.max(Math.min(b,d) - Math.max(a,c), 0); 
+        writer.write(String.valueOf(painted)); 
+        writer.close(); 
+    }
+}
