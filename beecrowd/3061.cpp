c383cd12fdc63714fb826cb54bf99b99143ba83a
Luiza de Melo <ludemelo5452@gmail.com>
2023-03-02 15:26:47 -0300

    dando TLE

--- beecrowd/3061.cpp
+++ beecrowd/3061.cpp
@@ -8,89 +8,91 @@ using namespace std;
 typedef vector<int> vi;
 
 vector<bool> vis;
-vector<vector<int>> adj; 
+vector<vector<int>> adj;
 vector<pair<int, int>> v;
 
 int n, m;
+queue<int> q;
 
-void dfs(int i)
+void dfs()
 {
-	vis[i] = true;
-	int x = v[i].first;
-	int y = v[i].second;
 
-	// acima
-	if (y - 1 >= 0)
-	{
-		int id = adj[x][y - 1];
-		if (id >= 0 && !vis[id])
-			dfs(id);
-	}
-	// direita
-	if (x + 1 < m)
-	{
-		int id = adj[x + 1][y];
-		cout << x << " " << y << " :" << adj[x+1][y] << endl;
-		if (id >= 0 && !vis[id])
-		{
-			dfs(id);
-			cout << "cheguei\n";
-		}
-	}
-	// abaixo
-	if (y + 1 < n)
-	{
-		int id = adj[x][y + 1];
-		if (id >= 0 && !vis[id])
-			dfs(id);
-	}
-	// esquerda
-	if (x - 1 >= 0)
-	{
-		int id = adj[x - 1][y];
-		if (id >= 0 && !vis[id])
-			dfs(id);
-	}
+    int i = q.front(); 
+    q.pop(); 
+    vis[i] = true;
+    int x = v[i].first;
+    int y = v[i].second;
+
+    // esquerda
+    if (y - 1 >= 0)
+    {
+        int id = adj[x][y - 1];
+        if (id >= 0 && !vis[id])
+            q.push(id); 
+    }
+    // abaixo
+    if (x + 1 < n)
+    {
+        int id = adj[x + 1][y];
+        if (id >= 0 && !vis[id])
+            q.push(id); 
+    }
+    // direita
+    if (y + 1 < m)
+    {
+        int id = adj[x][y + 1];
+        if (id >= 0 && !vis[id])
+            q.push(id); 
+    }
+    // acima
+    if (x - 1 >= 0)
+    {
+        int id = adj[x - 1][y];
+        if (id >= 0 && !vis[id])
+            q.push(id); 
+    }
 }
 
 int main()
 {
 
-	cin >> n >> m;
-	int id = 0;
-	adj.resize(n+1, vi(m+1)); 
+    cin >> n >> m;
+    int id = 0;
+    adj.resize(n + 1, vi(m + 1));
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			int x;
-			cin >> x;
-			if (x == 1)
-			{
-				adj[i][j] = id;
-				id++;
-				v.push_back({i, j});
-			}
-			else
-				adj[i][j] = -1;
-		}
-	}
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int x;
+            cin >> x;
+            if (x == 1)
+            {
+                adj[i][j] = id;
+                id++;
+                v.push_back({i, j});
+            }
+            else
+                adj[i][j] = -1;
+        }
+    }
 
-	vis.resize(v.size(), false);
+    vis.resize(v.size(), false);
 
-	int ans = 0;
+    int ans = 0;
 
-	for (int i = 0; i < v.size(); i++)
-	{
-		if (!vis[i])
-		{
-			dfs(i);
-			ans++;
-		}
-	}
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (!vis[i])
+        {
+            q.push(i); 
+            while (!q.empty())
+                dfs();
+            ans++;
+        }
+    }
 
-	cout << ans << endl;
+    cout << ans << endl;
 
-	return 0;
+    return 0;
 }
